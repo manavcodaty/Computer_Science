b39e22d2a934e75765be9a26cbc3d5f264059afd
Manav Codaty <111678991+manavcodaty@users.noreply.github.com>
2024-02-19 12:18:05 +0400

    Add cinema booking functionality

---
+++ cinema_booking.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+arr = ([0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0])
+
+df = pd.DataFrame(arr)
+
+print(df)
+
+booking_row = int(input("Enter row number: "))
+booking_col = int(input("Enter column number: "))
+
+if df.iloc[booking_row, booking_col] == 0:
+    df.iloc[booking_row, booking_col] = 1
+    print("Booking successful")
+    print(df)
