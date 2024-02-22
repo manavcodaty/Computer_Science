fcee02a543843496b6405474d45e20d5ab50ab7a
Manav Codaty <111678991+manavcodaty@users.noreply.github.com>
2024-02-22 14:32:35 +0400

    Add seat booking functionality to cinema_booking.py

--- cinema_booking.py
+++ cinema_booking.py
@@ -1,15 +1,25 @@
 import pandas as pd
+import sys
 
 arr = ([0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0])
 
 df = pd.DataFrame(arr)
 
 print(df)
+def make_booking():
+    booking_row = int(input("Enter row number: "))
+    booking_col = int(input("Enter column number: "))
 
-booking_row = int(input("Enter row number: "))
-booking_col = int(input("Enter column number: "))
-
-if df.iloc[booking_row, booking_col] == 0:
-    df.iloc[booking_row, booking_col] = 1
-    print("Booking successful")
-    print(df)
+    if df.iloc[booking_row, booking_col] == 0:
+        df.iloc[booking_row, booking_col] = "B"
+        print("Booking successful")
+        print(df)
+    else:
+        print("Seat already booked")
+    
+print("Would you like to book another seat? (y/n)")
+another_booking = input()
+if another_booking == "y":
+    make_booking()
+else:
+    sys.exit(0.5)
