c9d96f603731d703acd0756ffb007fe815ce3086
Manav Codaty <111678991+manavcodaty@users.noreply.github.com>
2024-03-04 11:41:11 +0400

    Update file path in file_handling.py

--- file_handling.py
+++ file_handling.py
@@ -1,4 +1,4 @@
-File_handle = open("C:\Users\27mcodaty\Computer_Science\3a. Task1Data (3).txt", "r")
+File_handle = open("3a. Task1Data (3).txt", "r")
 read_line = File_handle.readline()
 
 while len(read_line) > 0:
