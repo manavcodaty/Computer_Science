afa0f7235f36a1d6bb3cd2b10a957e14a1d84bc7
Manav Codaty <111678991+manavcodaty@users.noreply.github.com>
2024-03-04 12:06:33 +0400

    Refactored file_append.py and added file_append_2.py for improved data appending

--- file_append.py
+++ file_append.py
@@ -1,12 +1,21 @@
+def main(choice):
+    while choice != -1:
+        msg = input("Enter the message to append: ")
+        file = open("hello_world.txt", "w")
+        file.write(msg)
+        print("Data appended successfully")
+        file = open("hello_world.txt", "r")
+        content = file.read()
+        print(content)
+        file.close()
+        main(choice)
 
-while msg != -1:
-    msg = input("Enter the message to append: ")
-    file = open("hello_world.txt", "w")
-    msg = input("Enter the message to append: ")
-    file.write(msg)
-    print("Data appended successfully")
-    file = open("hello_world.txt", "r")
-    content = file.read()
-    print(content)
-    file.close()
+
+    
+choice = input("Do you want to append data to the file? (1/-1), (y/n)): ")
+main(choice)
+
+
+
+
         
