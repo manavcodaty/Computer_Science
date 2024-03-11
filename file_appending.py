f15ba9c6449a5a642703ad30edcadd0ab6dc05cd
Manav Codaty <111678991+manavcodaty@users.noreply.github.com>
2024-03-11 11:05:38 +0400

    Add new files and delete old files

--- file_appending.py
+++
@@ -1,9 +0,0 @@
-file = open("3a. Task1Data (3).txt", "a")
-msg = input("Enter the message to append: ")
-file.write(msg)
-print("Data appended successfully")
-file = open("3a. Task1Data (3).txt", "r")
-content = file.read()
-print(content)
-
-file.close()
